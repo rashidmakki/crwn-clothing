fada1a1bf615f4c98a55c9fa03d44cf45b9440f7
RASHID MAKKI <https://github.com/rashidmakki>
2020-05-04 03:42:37 +0530

    broken our homepage into smaller component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage';
+import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
     <div>
-       <HomePage />
+       <HomePage /> 
     </div>
   );
 }
