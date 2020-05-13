85c382f976b7426fb5bfa84aef2c824a5c60edba
Rashid Makki <https://github.com/rashidmakki>
2020-05-13 19:03:17 +0530

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import FormInput from "../../components/form-input/form-input";
 import CustomButton from "../../components/custom-button/custom-button";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth,signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.scss";
 
 class SignIn extends React.Component {
@@ -14,10 +14,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-
-    this.setState({ email: "", password: "" });
+    const{email,password}=this.state;
+    try{
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({ email: "", password: "" });
+    }catch(error){
+      console.log(error);
+    }
+    
   };
 
   handleChange = (event) => {
