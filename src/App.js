6977e374f794188d66434350e3eaa103853f2140
Rashid Makki <rashidmakkiha_cse18@its.edu.in>
2020-05-25 22:16:40 +0530

    addind cartIcon and cartDropdown components to our application, also implemented cartReducer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.action";
 
 class App extends React.Component {
+  
   unsubscribeFromAuth = null;
 
   componentDidMount() {
