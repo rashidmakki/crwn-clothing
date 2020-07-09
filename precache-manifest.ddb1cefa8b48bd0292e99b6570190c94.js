889bc9f9ef484188e267b10e3d8514ff37f78437
Rashid Makki <rashidmakkiha_cse18@its.edu.in>
2020-07-09 22:54:20 +0530

    Updates

---
+++ precache-manifest.ddb1cefa8b48bd0292e99b6570190c94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce99fbc38b786329a7358ab2a816bbb4",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "2525b6a3d269e36434b3",
+    "url": "/crwn-clothing/static/css/main.fbf7e67a.chunk.css"
+  },
+  {
+    "revision": "bde65759f4ca23358f2c",
+    "url": "/crwn-clothing/static/js/2.c2d52924.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/crwn-clothing/static/js/2.c2d52924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2525b6a3d269e36434b3",
+    "url": "/crwn-clothing/static/js/main.b0ded282.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
